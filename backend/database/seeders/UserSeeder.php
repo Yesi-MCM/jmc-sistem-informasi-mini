49920073b432499e5366c45ea74aa215d184903f
Yesi-MCM <yesi.marketingcrossmedia@gmail.com>
2026-06-16 12:37:36 +0800

    fix: ganti CACHE_STORE dari array ke database agar captcha persisten di Vercel serverless

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -17,7 +17,7 @@ class UserSeeder extends Seeder
                 'role_id' => 1, // Superadmin
                 'name' => 'Super Admin',
                 'username' => 'superadmin',
-                'email' => 'superadmin@jmc-mini.local',
+                'email' => 'yesi.marketingcrossmedia@gmail.com',
                 'cellphone' => '+6281111111111',
                 'password' => Hash::make('password'),
                 'status' => 'active',
@@ -28,9 +28,9 @@ class UserSeeder extends Seeder
                 'id' => 2,
                 'employee_id' => 1, // Andri Eko
                 'role_id' => 2, // Manager HRD
-                'name' => 'Andri Eko Prasetyo',
+                'name' => 'Gunawan',
                 'username' => 'manager',
-                'email' => 'andri.eko@jmc-mini.local',
+                'email' => 'hrga@jmc.co.id',
                 'cellphone' => '+6282218458888',
                 'password' => Hash::make('password'),
                 'status' => 'active',
@@ -41,9 +41,9 @@ class UserSeeder extends Seeder
                 'id' => 3,
                 'employee_id' => 2, // Yessi Maria
                 'role_id' => 3, // Admin HRD
-                'name' => 'Yessi Maria',
+                'name' => 'Andri Oktavianus',
                 'username' => 'admin',
-                'email' => 'yessi.maria@jmc-mini.local',
+                'email' => 'andrioktavianus@gmail.com',
                 'cellphone' => '+6281234567890',
                 'password' => Hash::make('password'),
                 'status' => 'active',
