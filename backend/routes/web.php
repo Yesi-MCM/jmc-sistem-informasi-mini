95f1ffb13325e77285c7c65595ba73db4cf913b6
Yesi-MCM <yesi.marketingcrossmedia@gmail.com>
2026-06-16 11:53:57 +0800

    debug: tambahkan rute /api-debug untuk inspeksi perutean

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -9,3 +9,19 @@ Route::get('/', function () {
 Route::get('/api/documentation', function () {
     return view('swagger');
 });
+
+Route::get('/api-debug', function () {
+    return response()->json([
+        'request_uri' => $_SERVER['REQUEST_URI'] ?? null,
+        'script_name' => $_SERVER['SCRIPT_NAME'] ?? null,
+        'request_path' => request()->path(),
+        'base_url' => request()->getBaseUrl(),
+        'base_path' => base_path(),
+        'routes' => collect(Route::getRoutes())->map(function ($route) {
+            return [
+                'uri' => $route->uri(),
+                'methods' => $route->methods(),
+            ];
+        })->values(),
+    ]);
+});
